bfbc3d94812047e8ab89bbc8026ac8e93efe608c
petros <pit.trak@gmail.com>
2020-08-12 01:22:51 +0300

    Big - O of array operations

---
+++ arrays.js
@@ -0,0 +1,18 @@
+/*
+Arrays are Ordered Data Structured
+
+Insertion O(n) -In the beginning-
+Removal O(n) -In the beginning-
+Searching O(n)
+Access O(1)
+
+.push() O(1)
+.pop() O(1)
+.shift() O(n)
+.unshift() O(n)
+.concat() O(n)
+.slice() O(n)
+.splice() O(n)
+.sort() O(n* logN)
+.forEach() / .map() / .filter() / .reduce() O(n)
+*/
