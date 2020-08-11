5a5034babf102db813b7dc1cc470db1a9c3898b7
petros <pit.trak@gmail.com>
2020-08-12 00:39:15 +0300

    Added O(n^2) time complexity example and an image of all categories

--- add.js
+++ add.js
@@ -1,13 +1,24 @@
+// O(1) Constant time complexity
 const add = (n) => {
     return n * (n + 1) / 2;
 }
 
+// O(n) Linear time complexity
 const add2 = (n) => {
     let total = 0
     for (let i = 0; i <= n; i++) {
         total += i     
     }
     return total
+}
+
+// O(n^2) Exponential time complexity (Squared)
+const nestedFor = (n) => {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            console.log(i, j);          
+        }
+    }
 }
 
 const t1 = performance.now()
