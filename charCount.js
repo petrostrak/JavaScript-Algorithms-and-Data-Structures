627260040b58a46510a2353afd0cbfdf6a05fe4f
petros <pit.trak@gmail.com>
2020-08-12 14:49:41 +0300

    Solving a Problem - Refactoring

--- charCount.js
+++ charCount.js
@@ -21,3 +21,17 @@ const charCount = (string) =>{
 }
 
 console.log(charCount("Petros Trakadas"))
+
+// REFACTORING THE CODE
+
+const charCount1 = (str) => {
+    let obj = {}
+    for(let char of str){
+        if(/[a-z0-9]/.test(char)){
+            obj[char] = ++obj[char] || 1;
+        }
+        return obj
+    }
+}
+
+console.log(charCount1("Petros Trakadas"))
